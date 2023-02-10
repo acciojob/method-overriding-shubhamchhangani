627cccf39a4d527cb5ce80172817bddf893c9e24
Shubham Chhangani <shubhamchhangani1998@gmail.com>
2023-02-10 11:09:32 +0530

    another try

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -4,21 +4,6 @@ import static org.junit.Assert.*;
 import org.junit.jupiter.api.Test;
 
 public class TestCases {
-    public static void main(String[] args) {
-        B b = new B();
-        b.meth();
-        b.meth();
-    }
+
 }
 
-class A{
-    public String meth(){
-        return "Invoking method from class A";
-    }
-}
-
-class B extends A{
-    public String meth(){
-        return "Method is overridden in Extendend class B";
-    }
-}
